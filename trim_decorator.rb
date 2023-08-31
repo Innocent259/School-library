57b14ab208bfac14490923c1f33bd9e26731f1d5
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-08-31 19:46:46 +0200

    Correct typos in naming

--- trim_decorator.rb
+++ trim_decorator.rb
@@ -1,6 +1,6 @@
 require_relative 'base_decorator'
 
-class TrimDecorator < Base_decorator
+class TrimDecorator < BaseDecorator
   def correct_name
     given_name = @nameable.correct_name
     given_name.length > 10 ? given_name[0..9] : given_name
