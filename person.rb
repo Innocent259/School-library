73726c2849eb90c6bec556303df30f79eeca8484
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-09-01 17:27:03 +0200

    Resolve linters errors

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_accessor :name :age :rentals
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name: 'unknown', parent_permission: true)
     super()
