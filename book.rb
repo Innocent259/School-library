22dc7d4a27a9027c0613768a89148cc73c29dc48
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-09-01 18:01:09 +0200

    Update add_rental method

--- book.rb
+++ book.rb
@@ -7,7 +7,9 @@ class Book
     @rentals = []
   end
 
-  def add_rental(rental)
-    @rentals.push(rental)
+  def add_rental(date, person)
+    rental = Rental.new(date, self, person)
+    @rentals << rental
+    rental
   end
 end
