4926238a189ab074dd21a96261dbbe7a0bd2a02f
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-09-02 22:17:35 +0200

    Update teacher's class

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
-require './person'
+require_relative 'person'
 
 class Teacher < Person
   attr_accessor :id, :specialization
 
-  def initialize(name, age, specialization)
-    super(name, age)
-    @specialization = specialization
+  def initialize(age, name, specialization)
+    super(age, name)
     @id = Random.rand(1..1000)
+    @specialization = specialization
   end
 
   def can_use_services?
