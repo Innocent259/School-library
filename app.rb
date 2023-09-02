16394db0f159749d679f8d2d2d5cd0d075918b47
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-09-02 17:48:41 +0200

    Update app file

--- app.rb
+++ app.rb
@@ -65,98 +65,13 @@ class App
     rentals.each { |rental| puts "#{rental.book.title} by #{rental.book.author} on #{rental.date}" }
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity
-  def run
-    puts 'Welcome to the Library Management App!'
-    loop do
-      display_menu
-      choice = gets.chomp.to_i
-
-      case choice
-      when 1 then list_all_books
-      when 2 then list_all_people
-      when 3 then create_teacher
-      when 4 then create_student
-      when 5 then create_book_prompt
-      when 6 then create_rental_prompt
-      when 7 then list_rentals_prompt
-      when 8
-        puts 'Goodbye!'
-        break
-      else
-        handle_invalid_choice
-      end
-    end
-  end
-  # rubocop:enable Metrics/CyclomaticComplexity
-
   private
 
-  def display_menu
-    puts "\nPlease choose an option:"
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a teacher'
-    puts '4. Create a student'
-    puts '5. Create a book'
-    puts '6. Create a rental'
-    puts '7. List rentals for a person'
-    puts '8. Quit'
-  end
-
-  def find_person_by_id(_person_id)
-    @people.find { |person| person.classroom == classroom }
+  def find_person_by_id(person_id)
+    @people.find { |person| person.id == person_id }
   end
 
   def find_book_by_title(book_title)
     @books.find { |book| book.title == book_title }
-  end
-
-  def handle_invalid_choice
-    puts 'Invalid choice. Please select a valid option.'
-  end
-
-  def create_teacher
-    puts "Enter teacher's age:"
-    age = gets.chomp.to_i
-    puts "Enter teacher's name:"
-    name = gets.chomp
-    puts "Enter teacher's specialization:"
-    specialization = gets.chomp
-    create_person(age, name, 'teacher', specialization: specialization)
-  end
-
-  def create_student
-    puts "Enter student's name:"
-    name = gets.chomp
-    puts "Enter student's age:"
-    age = gets.chomp.to_i
-    puts "Enter student's classroom:"
-    classroom = gets.chomp
-    create_person(age, name, 'student', classroom: classroom)
-  end
-
-  def create_book_prompt
-    puts 'Enter book title:'
-    title = gets.chomp
-    puts 'Enter book author:'
-    author = gets.chomp
-    create_book(title, author)
-  end
-
-  def create_rental_prompt
-    puts 'Enter rental date (YYYY-MM-DD):'
-    date = gets.chomp
-    puts 'Enter person ID:'
-    person_id = gets.chomp.to_i
-    puts 'Enter book title:'
-    book_title = gets.chomp
-    create_rental(date, person_id, book_title)
-  end
-
-  def list_rentals_prompt
-    puts 'Enter person ID:'
-    person_id = gets.chomp.to_i
-    list_rentals_for_person(person_id)
   end
 end
