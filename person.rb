4e871500db69ce80ffc6e62790442ab76b2c88cd
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-09-02 22:16:19 +0200

    Modify helpers

--- person.rb
+++ person.rb
@@ -1,39 +1,37 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age, :rentals
-  attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
 
-  def initialize(age, name, parent_permission: true)
-    super()
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
+    super()
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   def correct_name
     @name
   end
 
-  private
-
-  def of_age?
-    @age >= 18
-  end
-
-  public
-
-  def can_use_services?
-    @age >= 18 || @parent_permission
-  end
-
-  def add_rental(book, date)
-    rentals << Rental.new(book, self, date)
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 
   def to_s
     "[Person] Name: #{name}, ID: #{id}, Age: #{age}"
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
