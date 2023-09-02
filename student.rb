4b26c66ab85f9c8fdba2b9274f33a563076e49e1
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-09-02 16:56:15 +0200

    Remove the default value

--- student.rb
+++ student.rb
@@ -1,11 +1,11 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_accessor :classroom
 
-  def initialize(id, age, classroom, name: 'unknown', parent_permission: true)
+  def initialize(id, age, name, classroom, parent_permission: true)
     @classroom = classroom
-    super(id, age, name: name, parent_permission: parent_permission)
+    super(age, name, classroom, parent_permission: parent_permission)
   end
 
   def classroom=(classroom)
