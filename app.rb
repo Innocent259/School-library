d8e53f102aefd32707e0b31d544ddea2764ce59d
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-09-02 22:12:06 +0200

    Update the method for creating rentals

--- app.rb
+++ app.rb
@@ -11,14 +11,14 @@ class App
     @rentals = []
   end
 
-  def book_list
+  def list_of_books
     puts 'List of all books'
     @books.each do |book|
       puts "Title: \"#{book.title}\" , Author: #{book.author}"
     end
   end
 
-  def people_list
+  def list_of_people
     puts 'List of all people'
     @people.each do |person|
       puts person
@@ -51,7 +51,7 @@ class App
     end
   end
 
-  def all_renters
+  def all_person_who_can_rent
     @people.each_with_index do |person, index|
       if person.is_a?(Student)
         puts "#{index}) [Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
@@ -63,19 +63,19 @@ class App
     end
   end
 
-  def rented_book(selection_id)
+  def selected_book_for_rental(selection_id)
     return @books[selection_id].id unless selection_id.negative? || selection_id > @books.length
 
     puts 'Invalid Selection. Please choose a valid index'
   end
 
-  def renter_person(selection_id)
+  def selected_person_who_will_rent(selection_id)
     return @people[selection_id].id unless selection_id.negative? || selection_id > @people.length
 
     puts 'Invalid Selection. Please choose a valid index'
   end
 
-  def view_book_by_renters(id)
+  def display_book_rented_by_person(id)
     rented_books_by_id = @rentals.select { |rental| rental.person == id.to_i }
 
     if rented_books_by_id.empty?
@@ -88,20 +88,20 @@ class App
     end
   end
 
-  def create_person
+  def create_person_with_input
     print 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
     input_user = gets.chomp.to_i
     case input_user
     when 1
-      create_student_with_one
+      create_student_with_input
     when 2
-      create_teacher_with_three
+      create_teacher_with_input
     else
       puts 'Invalid input'
     end
   end
 
-  def create_student_with_one
+  def create_student_with_input
     puts 'Generating Student...'
     print 'Student Age:'
     age = gets.chomp.to_i
@@ -114,7 +114,7 @@ class App
     puts 'Student created successfully'
   end
 
-  def create_teacher_with_three
+  def create_teacher_with_input
     puts 'Generating Teacher...'
     print 'Teacher Age:'
     age = gets.chomp.to_i
@@ -126,7 +126,7 @@ class App
     puts 'Teacher created successfully'
   end
 
-  def create_a_book
+  def create_book_with_input
     puts 'Provide book information'
     print 'Book Title: '
     title = gets.chomp
@@ -136,20 +136,16 @@ class App
     puts 'Book created successfully'
   end
 
-  def create_a_rental
+  def create_rental_with_input
     puts 'Provide rental information'
     puts 'Select book from the following list by number: '
     books_for_rent
     book_id = gets.chomp.to_i
-    book = rented_book(book_id)
-
+    book = selected_book_for_rental(book_id)
     puts 'Select person from the following list by number (not id): '
-    all_renters
+    all_person_who_can_rent
     person_id = gets.chomp.to_i
-    person = renter_person(person_id)
-
-    puts "Selected person: #{person.name} (ID: #{person.id}), Age: #{person.age}, Role: #{person.class}"
-
+    person = selected_person_who_will_rent(person_id)
     print 'Date: '
     date = gets.chomp.to_s
     create_rental(date, book, person)
@@ -167,10 +163,10 @@ class App
     puts '7 - Exit'
   end
 
-  def search_rental_by_id
+  def search_rented_book_with_id
     print 'ID of the person: '
     id = gets.chomp.to_i
     puts 'List all rentals'
-    view_book_by_renters(id)
+    display_book_rented_by_person(id)
   end
 end
